7fae0264dd0b250a9bc238903586299f4e16dc24
egor187 <py.egor.py@gmail.com>
2021-03-06 16:28:44 +0300

    draft: draft model for 'Contracts' app

--- contracts/models.py
+++ contracts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse
+from apartments.models import Apartments
+from tenants.models import Tenants
+from users.models import CustomUser
 
-# Create your models here.
+
+# class Contracts(models.Model):
+#     apartment = models.ForeignKey(Apartments, on_delete=models.CASCADE, related_name='apartment')
+#     concluded_at = models.DateField(auto_now_add=True)
+#     expiration_date = models.DateField()
+#     owner = models.ForeignKey(Apartments, on_delete=models.CASCADE, to_field='owner', related_name='owner')
+#     tenant = models.ForeignKey(Tenants, on_delete=models.CASCADE, verbose_name='tenant name')
+#
+#     def __str__(self):
+#         return self.concluded_at
+#
+#     def get_absolute_url(self):
+#         return reverse('contracts.views.ContractsDetailView', args=[str(self.id)])
