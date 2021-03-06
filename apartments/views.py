7bd0958e22251c29b94e029abaf60368c6c2da7c
egor187 <py.egor.py@gmail.com>
2021-03-06 16:24:56 +0300

    views: create views for 'apartments' and 'users' app's

--- apartments/views.py
+++ apartments/views.py
@@ -1,3 +1,50 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from apartments.models import Apartments
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
-# Create your views here.
+
+class ApartmentsIndexView(ListView):
+    model = Apartments
+    template_name = 'apartment_index.html'
+    context_object_name = 'apartments_list'
+
+    def get_queryset(self):
+        queryset = Apartments.objects.filter(owner__pk = self.request.user.pk)
+        return queryset
+
+
+class ApartmentsDetailView(DetailView):
+    model = Apartments
+    template_name = 'apartment_detail.html'
+    context_object_name = 'apartment'
+
+
+class ApartmentsUpdateView(UpdateView):
+    model = Apartments
+    fields = '__all__'
+    template_name = 'apartment_update.html'
+
+
+class ApartmentsCreateView(LoginRequiredMixin, CreateView):
+    model = Apartments
+    fields = [
+        'current_tenant',
+        'address',
+        'size',
+    ]
+    template_name = 'apartment_create.html'
+    success_url = reverse_lazy('apartments:apartment_index')
+
+    # override class-method to achieve auto increment form field
+    # "owner" with current autheticated user
+    def form_valid(self, form):
+        # attribute "user" contain current login user by LoginRequiredMixin
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class ApartmentsDeleteView(DeleteView):
+    model = Apartments
--- users/views.py
+++ users/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
 from users.models import CustomUser
+from apartments.models import Apartments
+from payments.models import Payments
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from users.forms import CustomUserCreationForm, CustomUserUpdateForm
 
 
-class CustomUserListView(ListView):
+class CustomUserIndexView(ListView):
     model = CustomUser
-    # template_name =
+    template_name = 'index.html'
+    context_object_name = 'rantier_list'
 
-class CustomUserDetailView(DetailView):
+class CustomUserDetailView(LoginRequiredMixin, DetailView):
     model = CustomUser
-    # template_name = pass
+    context_object_name = 'rantier'
+    template_name = 'user_detail.html'
+
+    # def get_queryset(self, **kwargs):
+    #     queryset = CustomUser.objects.filter(pk=self.kwargs['pk'])
+    #     return queryset
+
+
+class CustomUserCreateView(CreateView):
+    model = CustomUser
+    form_class = CustomUserCreationForm
+    template_name = 'user_create.html'
+    success_url = reverse_lazy('users:login')
+
+
+class CustomUserUpdateView(UpdateView):
+    model = CustomUser
+    form_class = CustomUserUpdateForm
+    template_name = 'user_update.html'
