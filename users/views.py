e32f5fa604b2a05c99b730b07ccd16f460a36364
egor187 <py.egor.py@gmail.com>
2021-03-08 15:42:58 +0300

    views: 1) override super method dispatch() for protect access current user (from session midware) to "detail" page of others rantiers; 2) override super method get_context_data() for add new context with Contracts model (to render related to apartments contract purpose)

--- users/views.py
+++ users/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from users.models import CustomUser
+from contracts.models import Contracts
+from django.contrib import messages
+from django.shortcuts import redirect
 from apartments.models import Apartments
 from payments.models import Payments
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -19,9 +22,33 @@ class CustomUserDetailView(LoginRequiredMixin, DetailView):
     context_object_name = 'rantier'
     template_name = 'user_detail.html'
 
-    # def get_queryset(self, **kwargs):
-    #     queryset = CustomUser.objects.filter(pk=self.kwargs['pk'])
-    #     return queryset
+    not_allowed_message = "You can't see details about another rantier"
+    redirect_url = reverse_lazy('users:user_index')
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Prohibit access to another rantiers detail page
+        """
+        if self.request.user.pk != kwargs['pk']:
+            messages.error(request, self.not_allowed_message)
+            return redirect(self.redirect_url)
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        """
+        Add Contract instance to context
+        :param kwargs:
+        :return: new 'key/value' pair in context dict
+        """
+        context = super().get_context_data()
+
+        # filtering model instance to url_dispatcher kwarg from url
+        context['contract'] = Contracts.objects.filter(apartment__owner__pk=self.kwargs['pk'])
+
+        # Another realization: return contracts filtered by current user in session
+        # from self.request (more secured). Useless with overriden dispatch super-class merhod
+        # context['contract'] = Contracts.objects.filter(apartment__owner__pk=self.request.user.pk)
+        return context
 
 
 class CustomUserCreateView(CreateView):
