c9c41d7a41f77e6fc03c7c87c2bf944f0725c5b7
egor187 <py.egor.py@gmail.com>
2021-03-09 12:39:53 +0300

    models: syntax fix

--- apartments/models.py
+++ apartments/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 from users.models import CustomUser
-# from tenants.models import Tenants
 from django.urls import reverse
 
 
 class Apartments(models.Model):
     owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    # current_tenant = models.ForeignKey(Tenants, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=200)
     size = models.IntegerField()
 
