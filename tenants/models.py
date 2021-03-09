244ec2c5a8e8299d2386199f237a8bd798e153dd
egor187 <py.egor.py@gmail.com>
2021-03-09 12:39:44 +0300

    models: set absolute_url to Tenants model

--- tenants/models.py
+++ tenants/models.py
@@ -1,18 +1,15 @@
 from django.db import models
-# from contracts.models import Contracts
-# from payments.models import Payments
 from django.urls import reverse
 
 
 class Tenants(models.Model):
     name = models.CharField('tenant name', max_length=120)
     email = models.EmailField()
-    # contract = models.ForeignKey(Contracts, on_delete=models.SET_NULL)
-    # payments = models.ForeignKey(Payments, on_delete=models.SET_NULL)
+
 
 
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #     return reverse('tenants.views.TenantDetailView', args=[str(self.pk)])
+    def get_absolute_url(self):
+        return reverse('tenants:tenant_detail', args=[self.pk])
