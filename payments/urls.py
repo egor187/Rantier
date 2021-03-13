feafbe8647c5cb8dcc55815f7849c1e87eb53481
egor187 <py.egor.py@gmail.com>
2021-03-13 23:07:04 +0300

    urls: add "create" route for "payments" app

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from payments.views import PaymentDetailView
+from payments.views import PaymentDetailView, PaymentCreateView
 
 
 app_name = 'payments'
 
 urlpatterns = [
-    path('<int:pk>/', PaymentDetailView.as_view(), name='payment_detail')
+    path('<int:pk>/', PaymentDetailView.as_view(), name='payment_detail'),
+    path('create/', PaymentCreateView.as_view(), name='payment_create'),
 ]
