c0b18b7e8c1e64787c7fc1190f31d836827f3b81
egor187 <py.egor.py@gmail.com>
2021-03-13 23:02:55 +0300

    views: add new class-based view for "payment" app (for create "payment" object)

--- payments/views.py
+++ payments/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from payments.models import Payments
 from django.views.generic import DetailView
+from django.views.generic.edit import CreateView
+from payments.forms import PaymentCreationForm
 
 
 class PaymentDetailView(DetailView):
     model = Payments
     template_name = 'payment_detail.html'
     context_object_name = 'payment'
+
+
+class PaymentCreateView(CreateView):
+    model = Payments
+    form_class = PaymentCreationForm
+    template_name = 'payment_create.html'
