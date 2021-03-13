ac96d50bc3f634a1e2ee96d8419ed35ee15d6c93
egor187 <py.egor.py@gmail.com>
2021-03-13 23:04:57 +0300

    models: refactor (override class-method save()) for automatic isntance of Payment model in moment (+"payment period" of Contract object) with contract creation

--- contracts/models.py
+++ contracts/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.urls import reverse
 from apartments.models import Apartments
 from tenants.models import Tenants
+from datetime import datetime, timedelta
+
 
 
 class Contracts(models.Model):
     apartment = models.ForeignKey(Apartments, on_delete=models.CASCADE, related_name='contracts')
     concluded_at = models.DateField(auto_now_add=True)
     expiration_date = models.DateField()
+    # payment_period = models.IntegerField()  # field for more flex payment auto-creation (set timedelta value from this field)
     cost = models.IntegerField()
     tenant = models.ForeignKey(Tenants, on_delete=models.CASCADE, verbose_name='tenant name')
 
@@ -16,3 +19,20 @@ class Contracts(models.Model):
 
     # def get_absolute_url(self):
     #     return reverse('contracts:contract_detail', args=[str(self.id)])
+
+    def save(self, *args, **kwargs):
+        """
+        Override class method for auto-creation first payment with new contract
+        :param args:
+        :param kwargs:
+        :return: None
+        """
+        from payments.models import Payments
+        super().save(*args, **kwargs)
+        Payments.objects.create(
+            contract=self,
+            description='Pay needed',
+            rent_payment=self.cost,
+            created_at=self.concluded_at + timedelta(30)
+            # created_at=self.concluded_at + timedelta(self.payment_period)  # more flex behavior!!
+        )
