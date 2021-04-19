944791765b919e5bffece6cfc3f495102337dd20
egor187 <py.egor.py@gmail.com>
2021-04-19 10:25:31 +0300

    feature: add django-summernote

--- Rantier/settings.py
+++ Rantier/settings.py
@@ -48,7 +48,8 @@ INSTALLED_APPS = [
     'contracts',
     'payments',
     'tenants',
-    'bootstrap4'
+    'bootstrap4',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
